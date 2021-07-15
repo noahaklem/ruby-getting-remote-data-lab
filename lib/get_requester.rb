0bbfd654637b92954bc3bbb25d136f609442647a
Noah A Klem <noahaklem@gmail.com>
2021-07-15 10:09:36 -0600

    commit tree 3b38ea7ba0be38791d5e961369a2c88b9cda7149

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
-# Write your code here
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+class GetRequester
+
+ def initialize(url)
+    @url = url
+ end
+
+ def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+ end
+ 
+ def parse_json
+    data = JSON.parse(self.get_response_body)
+    data
+ end
+
+end
